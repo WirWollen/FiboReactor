e17ca9235cdf2604cfc572208c2cbb96666a4c5c
revyakin00 <revyakin@keeneye.pro>
2021-12-10 15:23:13 +0400

    RandomElementFactory and @Bean creation

--- src/main/java/com/example/FiboReact/configuration/BeanConfig.java
+++ src/main/java/com/example/FiboReact/configuration/BeanConfig.java
@@ -7,7 +7,7 @@ import com.example.FiboReact.components.ValueOfYAML;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//
+///
 @Configuration
 public class BeanConfig {
 
